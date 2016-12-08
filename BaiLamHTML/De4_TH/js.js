4356e3c91f9b19c3401e86d33c40d255f6986897
NguyenQuynh123 <nguyenvanquynh232@gmail.com>
2016-12-08 15:38:20 +0700

    Add files via upload

---
+++ BaiLamHTML/De4_TH/js.js
@@ -0,0 +1,31 @@
+﻿function select_TheHouseBunny() {
+    document.getElementById('movietrailers_TheHouseBunny').style.display = 'block';
+    document.getElementById('movietrailers_EagleEye').style.display = 'none';
+    document.getElementById('movietrailers_JourneyToTheCentre').style.display = 'none';
+}
+function select_EagleEye() {
+    document.getElementById('movietrailers_TheHouseBunny').style.display = 'none';
+    document.getElementById('movietrailers_EagleEye').style.display = 'block';
+    document.getElementById('movietrailers_JourneyToTheCentre').style.display = 'none';
+}
+function select_Journey() {
+    document.getElementById('movietrailers_TheHouseBunny').style.display = 'none';
+    document.getElementById('movietrailers_EagleEye').style.display = 'none';
+    document.getElementById('movietrailers_JourneyToTheCentre').style.display = 'block';
+}
+function tangLen5Sao_reportEE() {
+    document.getElementById('report_3sao').style.display = 'none';
+    document.getElementById('report_5sao').style.display = 'block';
+}
+function giamXuong3Sao_reportEE() {
+    document.getElementById('report_3sao').style.display = 'block';
+    document.getElementById('report_5sao').style.display = 'none';
+}
+function tangLen4Sao_reportHB() {
+    document.getElementById('report_2sao').style.display = 'none';
+    document.getElementById('report_4sao').style.display = 'block';
+}
+function giamXuong2Sao_reportHB() {
+    document.getElementById('report_2sao').style.display = 'block';
+    document.getElementById('report_4sao').style.display = 'none';
+}
